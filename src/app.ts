22896de115f3d9b026f56d41017b4e1f8e4e4bca
eu-neita <neitass.music@gmail.com>
2023-08-13 20:10:26 -0300

    --allow-empty

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import productController from './Controller/product.controller';
-import orderController from './Controller/order.controller';
-import userController from './Controller/user.controller';
+import productController from './controller/product.controller';
+import orderController from './controller/order.controller';
+import userController from './controller/user.controller';
 import productMiddleware from './product.middleware';
 
 const app = express();
