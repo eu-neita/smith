f118a122bc30790c5b4404fac832e5dc534d7b23
eu-neita <neitass.music@gmail.com>
2023-08-13 16:24:29 -0300

    fix: jwt sing values

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,22 +6,21 @@ import { UserLogin } from '../types/User';
 import { UserServiceResponse } from '../types/Userservice';
 
 const secret = process.env.JWT_SECRET as string;
-type Token = string;
-async function loginVerificate(credentials: UserLogin): Promise<UserServiceResponse<Token>> {
+async function loginVerificate(credentials: UserLogin): Promise<UserServiceResponse<object>> {
   const response = await UserModel.findOne({ where: { username: credentials.username } });
   if (response === null || !bcrypt
     .compareSync(credentials.password, response?.dataValues.password as string)) {
     return { status: 401, data: { message: 'Username or password invalid' } };
   }
   const token = jwt.sign(
-    { username: credentials.username, password: credentials.password },
+    { id: response.dataValues.id, username: credentials.username },
     secret,
     {
       algorithm: 'HS256',
       expiresIn: 86400,
     },
   );
-  return { status: 201, data: token };
+  return { status: 200, data: { token } };
 }
 
 export default { 
